675e6bdb32cad42fbf03890dd9a38fd2225c9881
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-07-21 07:32:00 +0700

    last commit

--- pages/api/fasilitator/pembetulan-nama/[employeeNumber]/information.js
+++ pages/api/fasilitator/pembetulan-nama/[employeeNumber]/information.js
@@ -1,14 +1,6 @@
 import nc from "next-connect";
-<<<<<<< HEAD
-import { information } from "../../../../../controller/sapk.controller";
-import auth from "../../../../../middleware/auth";
-const handler = nc();
-
-export default handler.use(auth).get(information);
-=======
 import auth from "../../../../../middleware/auth";
 const handler = nc();
 import { informasiPembetulanNama } from "../../../../../controller/sapk.controller";
 
 export default handler.use(auth).get(informasiPembetulanNama);
->>>>>>> 6b81ccfb331abffb9cb5caaab3e90166ea9d52c0
--- pages/api/fasilitator/request-pembetulan-nama/index.js
+++ pages/api/fasilitator/request-pembetulan-nama/index.js
@@ -1,5 +1,16 @@
 import nc from "next-connect";
+import {
+    index,
+    patch,
+    post,
+    remove
+} from "../../../../controller/request-pembetulan-nama.controller";
 import auth from "../../../../middleware/auth";
 const handler = nc();
 
-export default handler.use(auth).get().patch().delete();
+export default handler
+    .use(auth)
+    .get(index)
+    .patch(patch)
+    .delete(remove)
+    .post(post);
