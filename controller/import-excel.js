a1166b912bd5869d8c41fe3bb2055d4cdf53e7ab
Iput <taufiqurrohman.suwarto@gmail.com>
2022-09-22 05:28:15 +0700

    last commit

--- controller/import-excel.js
+++ controller/import-excel.js
@@ -1,6 +1,6 @@
 const { default: prisma } = require("../lib/prisma");
 const AdmZip = require("adm-zip");
-const { chunk, groupBy } = require("lodash");
+const { groupBy, chunk } = require("lodash");
 const xlsx = require("xlsx");
 const stream = require("stream");
 import moment from "moment";
@@ -16,86 +16,57 @@ const index = async (req, res) => {
 
             // change format result created_at to dd-mm-yyyy
             const data = result.map((item) => {
+                const date = new Date(item?.created_at);
+                const day = date?.getDate();
+                const month = date?.getMonth() + 1;
+                const year = date?.getFullYear();
+                const newDate = `${day}-${month}-${year}`;
+
+                // format date to dd-mm-yyyy locale id
+
                 return {
                     ...item,
-                    tgl_dibuat: moment(item?.created_at).format("DD-MM-YYYY")
+                    tgl_dibuat: moment(item?.created_at)
+                        .locale("id")
+                        .format("DD-MM-YYYY")
                 };
             });
 
-            const dataGroupByDate = groupBy(data, "tgl_dibuat");
+            // group data by tgl_dibuat
+            const groupData = groupBy(data, "tgl_dibuat");
 
             const zipAll = new AdmZip();
-            const zipExcel = new AdmZip();
+
+            let excel = [];
             // iterate dataGroupByDate to create zip file
-            Object.keys(dataGroupByDate).forEach((key) => {
-                const data = dataGroupByDate[key];
+            Object.keys(groupData).forEach((key, indexKey) => {
+                const data = groupData[key];
+                // filter data by tgl_dibuat
+
+                excel[indexKey] = new AdmZip();
+
                 const unor = data?.sort(
-                    (a, b) => a?.tgl_dibuat - b?.tgl_dibuat
+                    (a, b) => a?.created_at - b?.created_at
                 );
                 const splitUnor = chunk(unor, 100);
 
-                splitUnor.forEach((item, index) => {
+                splitUnor?.forEach((item, index) => {
                     const wb = xlsx.utils.book_new();
                     const ws = xlsx.utils.json_to_sheet(item);
                     xlsx.utils.book_append_sheet(wb, ws, "Sheet1");
                     const buffer = xlsx.write(wb, { type: "buffer" });
-                    zipExcel.addFile(`data-${key}-${index}.xlsx`, buffer);
+                    excel[indexKey].addFile(
+                        `data-${key}-${index}.xlsx`,
+                        buffer
+                    );
                 });
 
-                // then all goes here
-
-                zipAll.addFile(`data-${key}.zip`, zipExcel.toBuffer());
-            });
-
-            // filter result by jfu id is not empty order by created_at desc and split 100 data
-            const dataRiwayatUnor = data?.sort(
-                (a, b) => b?.created_at - a?.created_at
-            );
-
-            const unor = chunk(dataRiwayatUnor, 100);
-
-            const filterJfu = data
-                .filter((item) => !!item?.jfu_id)
-                .sort((a, b) => b?.created_at - a?.created_at);
-
-            const jfu = chunk(filterJfu, 400);
-
-            const filterJf = data
-                .filter((item) => !!item?.jft_id)
-                .sort((a, b) => b?.created_at - a?.created_at);
-
-            const jft = chunk(filterJf, 400);
-
-            const filterStruktural = data
-                .filter((item) => !item?.jft_id && !item?.jfu_id)
-                .sort((a, b) => b?.created_at - a?.created_at);
-
-            const struktural = chunk(filterStruktural, 400);
-
-            const myData = { unor, jfu, jft, struktural };
-
-            const zip = new AdmZip();
-
-            // looping mydata by properties
-
-            Object.keys(myData).forEach((key) => {
-                const data = myData[key];
-
-                data.forEach((item, index) => {
-                    const workbook = xlsx.utils.book_new();
-                    const worksheet = xlsx.utils.json_to_sheet(item);
-                    xlsx.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-                    // save as buffer
-                    const buffer = xlsx.write(workbook, {
-                        type: "buffer",
-                        bookType: "xlsx"
-                    });
-
-                    zip.addFile(`${key}-${index}.xlsx`, buffer);
+                excel.forEach((item) => {
+                    zipAll.addFile(`${key}.zip`, item.toBuffer());
                 });
             });
 
-            const willSend = zip.toBuffer();
+            const willSend = zipAll.toBuffer();
             const readStream = stream.PassThrough();
 
             readStream.end(willSend);
