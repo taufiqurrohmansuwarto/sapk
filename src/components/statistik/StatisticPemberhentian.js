b33b85d6e468ede9d5eb0bd55205174288f55850
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2023-04-07 07:09:40 +0700

    last commit

--- src/components/statistik/StatisticPemberhentian.js
+++ src/components/statistik/StatisticPemberhentian.js
@@ -1,7 +1,14 @@
 import { statistikPemberhentian } from "@/services/fasilitator.service";
-import { Bar } from "@ant-design/plots";
+import { Bar, Treemap } from "@ant-design/plots";
 import { useQuery } from "@tanstack/react-query";
-import { Card } from "antd";
+import { Card, Col, Row } from "antd";
+
+const serializeData = (data) => {
+    return data?.map((item) => ({
+        name: item.type,
+        value: item.value
+    }));
+};
 
 function StatisticPemberhentian() {
     const { data, isLoading } = useQuery(
@@ -40,9 +47,36 @@ function StatisticPemberhentian() {
         }
     };
 
+    const treeMapConfig = {
+        data: {
+            name: "root",
+            children: serializeData(data?.status_usulan)
+        },
+        colorField: "name"
+    };
+
+    const treeMapConfig2 = {
+        data: {
+            name: "root",
+            children: serializeData(data?.sub_layanan)
+        },
+        colorField: "name"
+    };
+
     return (
         <Card loading={isLoading} title="Pemberhentian">
+            {/* {JSON.stringify(serializeData(data?.status_usulan))} */}
             {data && (
+                <Row gutter={[8, 16]}>
+                    <Col xs={24} md={12}>
+                        <Treemap {...treeMapConfig} />
+                    </Col>
+                    <Col xs={24} md={12}>
+                        <Treemap {...treeMapConfig2} />
+                    </Col>
+                </Row>
+            )}
+            {/* {data && (
                 <div>
                     <div>
                         <Bar {...config} />
@@ -51,7 +85,7 @@ function StatisticPemberhentian() {
                         <Bar {...config2} />
                     </div>
                 </div>
-            )}
+            )} */}
         </Card>
     );
 }
